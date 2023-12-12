d4cfc99d8eacb5f5dac84343b42f15f838a11897
annmaryajith <118360733+annmaryajith@users.noreply.github.com>
2023-12-12 16:33:28 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,64 +1,33 @@
-package com.example.sqllite;
+package com.example.activity1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText name,lname;
+    private Button button;
 
-    TextView tv1;
-    EditText et1,et2,et3;
-    Button bt1,bt2,bt3,bt4;
-    String rno;
-    String name;
-    String dept;
-    SQLiteDatabase db;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        tv1 = findViewById(R.id.tv1);
-        et1 = findViewById(R.id.et1);
-        et2 = findViewById(R.id.et2);
-        et3 = findViewById(R.id.et3);
-        bt1 = findViewById(R.id.bt1);
-        bt2 = findViewById(R.id.bt2);
-        bt3 = findViewById(R.id.bt3);
-        bt4 = findViewById(R.id.bt4);
-        DbHelper dbHelper = new DbHelper(this);
-        db = dbHelper.getWritableDatabase();
-        db = dbHelper.getReadableDatabase();
-    }
-    public void onInsert(View view) {
-        rno = et1.getText().toString();
-        name = et2.getText().toString();
-        dept = et3.getText().toString();
-        if (rno.equals("") || name.equals("") || dept.equals("")) {
-            Toast.makeText(this,"please enter values",Toast.LENGTH_LONG).show();
-        }
-        else {
-            ContentValues values = new ContentValues();
-            values.put("rollno",rno);
-            values.put("name",name);
-            values.put("dept",dept);
-            db.insert("student",null,values);
-            Toast.makeText(this,"Inserted",Toast.LENGTH_LONG).show();
-        }
-    }
+        name= findViewById(R.id.name);
+        lname= findViewById(R.id.lname);
+        button= findViewById(R.id.button);
 
-    public void onUpdate(View view) {
-    }
+        button.setOnClickListener(v -> {
+            String fname=name.getText().toString();
+            String llname= lname.getText().toString();
 
-    public void onRead(View view) {
-    }
-
-    public void onDelete(View view) {
+            Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+            intent.putExtra("keyname", fname);
+            intent.putExtra("keylname", llname);
+            startActivity(intent);
+        });
     }
 }
--- MainActivity2.java
+++ MainActivity2.java
@@ -2,13 +2,24 @@ package com.example.activity1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+    private TextView name,lname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        name =findViewById(R.id.textname);
+        lname =findViewById(R.id.textage);
+
+        String fname = getIntent().getStringExtra("keyname");
+        String llname = getIntent().getStringExtra("keylname");
+
+        name.setText(fname);
+        lname.setText(llname);
     }
 }
